a9e5b2198ef83be7bc1090132b6c2bf10f1099a4
Askiki <55140687+Askiki@users.noreply.github.com>
2019-11-07 20:34:10 +0800

    5..1
    5.1

---
+++ WordCountReducer.java
@@ -0,0 +1,15 @@
+package com.study;
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WordCountReducer extends  Reducer<Text,IntWritable,Text,IntWritable>{
+    protected void reduce(Text k3,Iterable<IntWritable>v3,Context context)throws IOException,InterruptedException{
+        int total = 0;
+        for (IntWritable v:v3){
+            total += v.get();
+        }
+        context.write(k3,new IntWritable(total));
+    }
+}
